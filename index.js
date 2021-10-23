8b8f4cf3aedcf8b3757a7f9de5b0114c55c3c335
tuanstvn1215 <tuanstvn1215@gmail.com>
2021-10-23 10:41:16 +0700

    e

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', async (req, res) => {
             console.log(`Got response: ${res.statusCode}`)
         }
     )
-    res.send(res)
+    res.send(JSON.stringify(res))
 })
 app.listen(process.env.PORT, () => {
     console.log(process.env.PORT)
